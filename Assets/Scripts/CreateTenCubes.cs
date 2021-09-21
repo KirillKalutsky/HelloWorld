c35847a05ab54ad732b6cdaa4408ab6c2cb004e6
KirillKalutsky <kefirchik.2000@mail.ru>
2021-09-21 13:35:09 +0500

    hello world

---
+++ Assets/Scripts/CreateTenCubes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CreateTenCubes : MonoBehaviour
+{
+    List<GameObject> cubes = new List<GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cubes.Count < 10)
+        {
+            var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.transform.position = new Vector3(Random.Range(-9,9),Random.Range(-9,9),Random.Range(5, 10));
+            cubes.Add(cube);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            var cubeForDestroy = cubes[Random.Range(0,cubes.Count)];
+            cubes.Remove(cubeForDestroy);
+            Destroy(cubeForDestroy);
+        }
+    }
+}
---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    Light _light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Tab))
+            _light.enabled = !_light.enabled;
+    }
+}
